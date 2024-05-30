1af9b1822c5343b2c29178d1e40152f178373970
Ansh Garhewal <anshdevsin@gmail.com>
2024-05-30 15:14:54 +0530

    added GetUserById business logic

--- internal/gapi/get_user_by_id.go
+++ internal/gapi/get_user_by_id.go
@@ -4,9 +4,24 @@ import (
 	"context"
 
 	"github.com/ansh-devs/tc-assessment/protos"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // GetUserById implements protos.UserServiceServer.
-func (srv *UserService) GetUserById(_ context.Context, req *protos.GetUserByIdRequest) (resp *protos.GetUserByIdResponse, err error) {
-	panic("unimplemented")
+func (s *UserService) GetUserById(_ context.Context, req *protos.GetUserByIdRequest) (*protos.GetUserByIdResponse, error) {
+
+	user, err := s.Repository.GetUserById(int(req.GetId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &protos.GetUserByIdResponse{
+		User: &protos.User{
+			Id:     int64(user.ID),
+			FName:  user.FName,
+			Height: float32(user.Height),
+			City:   user.City,
+			Phone:  user.Phone,
+		}}, nil
 }
